de6d53c0f7b70327e1c51a5a97da44104f3bd9f2
Tunç <t.ozay98@gmail.com>
2026-02-06 13:46:48 +0300

    feature: remove custom tagger

--- lua/config/plugins/init.lua
+++ lua/config/plugins/init.lua
@@ -90,14 +90,4 @@ return {
       },
     },
   },
-
-  {
-    "romus204/go-tagger.nvim",
-    config = function()
-      require("go-tagger").setup({
-        skip_private = true,
-      })
-    end,
-  }
-
 }
